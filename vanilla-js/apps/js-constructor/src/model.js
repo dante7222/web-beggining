63c0530347d04a888504997ee66d5b4eda79c76a
Georgii Sorokin <44945227+dante7222@users.noreply.github.com>
2020-08-22 18:15:21 +0300

    Add image

--- vanilla-js/apps/js-constructor/src/model.js
+++ vanilla-js/apps/js-constructor/src/model.js
@@ -1,6 +1,10 @@
+import img from "./assets/fox.jpg"
+
+console.log(img)
 export const model = [
     {
         type: 'title', value: 'Test Title', options: {
+            tag: 'h2',
             styles: 'background: darkred; color: black; text-align:center;'
         }
     },
@@ -20,6 +24,13 @@ export const model = [
         ],
         options: {
             styles: 'padding: 1rem;'
+        }
+    },
+    {
+        type: 'img', value: img, options: {
+            styles: 'padding:2 rem 0; display:flex; justify-content: center;',
+            alt: 'my image',
+            imageStyles: 'width:500px; height:auto;'
         }
     }
 ]
